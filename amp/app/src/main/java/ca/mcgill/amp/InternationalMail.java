878f1ce3bce58d9e278d7a58a37cbeac6a56baca
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2016-02-21 18:44:27 -0500

    Add test for constructor and REGUALR_METERPOSTALINDICA for international mail

---
+++ amp/app/src/main/java/ca/mcgill/amp/InternationalMail.java
@@ -0,0 +1,41 @@
+package ca.mcgill.amp;
+
+/**
+ * Created by rtando1 on 21/02/2016.
+ */
+public class InternationalMail extends Mail {
+
+    double rateFor30;
+    double rateFor50;
+
+    double rateFor100;
+    double rateFor200;
+    double rateFor500;
+
+    public InternationalMail(double length, double height, double width, double weight, Item itemType) {
+        super(length, height, width, weight, itemType);
+
+        switch(itemType) {
+            case REGULAR_METERPOSTALINDICA:
+                this.rateFor30 = 2.36;
+                this.rateFor50 = 3.42;
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    double calculateStandard() {
+        // TODO Auto-generated method stub
+
+        return -1;
+    }
+
+    @Override
+    double calculateNonStandard() {
+        // TODO Auto-generated method stub
+
+        return -1;
+    }
+}
