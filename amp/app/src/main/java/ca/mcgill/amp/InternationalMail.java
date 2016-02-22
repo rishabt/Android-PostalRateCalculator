6cd199b7fd3db07953c6923bd6420a2baf3f4fa4
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2016-02-21 19:40:18 -0500

    passed test for weight >100 and <= 200 for non standard international

--- amp/app/src/main/java/ca/mcgill/amp/InternationalMail.java
+++ amp/app/src/main/java/ca/mcgill/amp/InternationalMail.java
@@ -57,8 +57,12 @@ public class InternationalMail extends Mail {
     @Override
     double calculateNonStandard() {
         // TODO Auto-generated method stub
+
         if(this.weight > 0 && this.weight <= 100){
             return this.rateFor100;
+        }
+        else if(this.weight > 100 && this.weight <= 200){
+            return this.rateFor200;
         }
         return -1;
     }
