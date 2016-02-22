f06aa4f9a5d84ad476a6f82c9db7f60c2c9847fb
deepaks4077 <deepaks4077@gmail.com>
2016-02-21 19:46:42 -0500

    Added functionality and passed testCanadaMailConstructor

--- amp/app/src/main/java/ca/mcgill/amp/CanadaMail.java
+++ amp/app/src/main/java/ca/mcgill/amp/CanadaMail.java
@@ -16,6 +16,36 @@ public class CanadaMail extends Mail {
 
     public CanadaMail(double length, double height, double width, double weight, Item itemType){
         super(length, height, width, weight, itemType);
+
+        switch(itemType){
+            case REGULAR_STAMPS :
+                this.rateFor30 = 0.85;
+                this.rateFor50 = 1.20;
+                break;
+            case REGULAR_METERPOSTALINDICA :
+                this.rateFor30 = 0.80;
+                this.rateFor50 = 1.19;
+                break;
+            case REGULAR_SINGLESTAMP :
+                this.rateFor30 = 1.00;
+                this.rateFor50 = 1.20;
+                break;
+            case OTHER_STAMP :
+                this.rateFor100 = 1.80;
+                this.rateFor200 = 2.95;
+                this.rateFor300 = 4.10;
+                this.rateFor400 = 4.70;
+                this.rateFor500 = 5.05;
+                break;
+            case OTHER_METERPOSTALINDICA :
+                this.rateFor100 = 1.71;
+                this.rateFor200 = 2.77;
+                this.rateFor300 = 3.89;
+                this.rateFor400 = 4.42;
+                this.rateFor500 = 4.74;
+                break;
+
+        }
     }
 
     @Override
